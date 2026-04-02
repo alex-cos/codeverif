9bd203568c0999725d9850edfa7d2a3794aba4b9
Alexandre Costantini <alexandre.costantini@exoalpha.com>
2026-04-02 22:36:12 +0200

    test: improve existing tests and add a new for concurrency

--- code_verif_test.go
+++ code_verif_test.go
@@ -1,11 +1,13 @@
 package codeverif_test
 
 import (
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/alex-cos/codeverif"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNominal(t *testing.T) {
@@ -14,51 +16,126 @@ func TestNominal(t *testing.T) {
 	cv := codeverif.New(4, 5*time.Second, 2)
 
 	code, err := cv.RequestCode("userID")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.NotEmpty(t, code)
 
 	err = cv.VerifyCode("userID", code)
 	assert.NoError(t, err)
 }
 
-func TestWithError(t *testing.T) {
+func TestRequestCodeThrottled(t *testing.T) {
 	t.Parallel()
 
 	cv := codeverif.New(4, 5*time.Second, 2)
 
+	_, err := cv.RequestCode("userID")
+	require.NoError(t, err)
+
+	_, err = cv.RequestCode("userID")
+	assert.ErrorIs(t, err, codeverif.ErrThrottled)
+}
+
+func TestVerifyCodeExpired(t *testing.T) {
+	t.Parallel()
+
+	cv := codeverif.New(4, 100*time.Millisecond, 2)
 	code, err := cv.RequestCode("userID")
-	assert.NoError(t, err)
-	assert.NotEmpty(t, code)
+	require.NoError(t, err)
 
-	err = cv.VerifyCode("userID", "12345")
-	assert.Error(t, err)
-
-	err = cv.VerifyCode("xxxxxx", code)
-	assert.Error(t, err)
-
-	code, err = cv.RequestCode("userID")
-	assert.Error(t, err)
-	assert.Empty(t, code)
-
-	cv = codeverif.New(4, time.Second, 2)
-	code, err = cv.RequestCode("userID")
-	assert.NoError(t, err)
-	assert.NotEmpty(t, code)
-
-	time.Sleep(1100 * time.Millisecond)
+	time.Sleep(150 * time.Millisecond)
 
 	err = cv.VerifyCode("userID", code)
-	assert.Error(t, err)
+	assert.ErrorIs(t, err, codeverif.ErrCodeExpired)
+}
 
-	cv = codeverif.New(4, time.Second, 2)
-	code, err = cv.RequestCode("userID")
-	assert.NoError(t, err)
-	assert.NotEmpty(t, code)
+func TestVerifyCodeNotFound(t *testing.T) {
+	t.Parallel()
 
-	err = cv.VerifyCode("userID", "12345")
-	assert.Error(t, err)
-	err = cv.VerifyCode("userID", "12345")
-	assert.Error(t, err)
+	cv := codeverif.New(4, 5*time.Second, 2)
+
+	err := cv.VerifyCode("nonexistent", "1234")
+	assert.ErrorIs(t, err, codeverif.ErrCodeExpired)
+}
+
+func TestVerifyInvalidCode(t *testing.T) {
+	t.Parallel()
+
+	cv := codeverif.New(4, 5*time.Second, 3)
+	code, err := cv.RequestCode("userID")
+	require.NoError(t, err)
+
+	err = cv.VerifyCode("userID", "0000")
+	assert.ErrorIs(t, err, codeverif.ErrInvalidCode)
+
 	err = cv.VerifyCode("userID", code)
-	assert.Error(t, err)
+	assert.NoError(t, err)
+}
+
+func TestVerifyTooManyAttempts(t *testing.T) {
+	t.Parallel()
+
+	cv := codeverif.New(4, 5*time.Second, 2)
+	code, err := cv.RequestCode("userID")
+	require.NoError(t, err)
+
+	err = cv.VerifyCode("userID", "0000")
+	assert.ErrorIs(t, err, codeverif.ErrInvalidCode)
+
+	err = cv.VerifyCode("userID", "0000")
+	assert.ErrorIs(t, err, codeverif.ErrInvalidCode)
+
+	err = cv.VerifyCode("userID", "0000")
+	assert.ErrorIs(t, err, codeverif.ErrTooManyAttempts)
+
+	err = cv.VerifyCode("userID", code)
+	assert.ErrorIs(t, err, codeverif.ErrTooManyAttempts)
+}
+
+func TestCodeFormat(t *testing.T) {
+	t.Parallel()
+
+	cv := codeverif.New(6, 5*time.Second, 2)
+	code, err := cv.RequestCode("userID")
+	require.NoError(t, err)
+
+	assert.Len(t, code, 6)
+	for _, c := range code {
+		assert.True(t, c >= '0' && c <= '9')
+	}
+}
+
+func TestInvalidCodeLength(t *testing.T) {
+	t.Parallel()
+
+	cv := codeverif.New(1, 5*time.Second, 2)
+	_, err := cv.RequestCode("userID")
+	assert.ErrorIs(t, err, codeverif.ErrInvalidLength)
+}
+
+func TestConcurrentVerify(t *testing.T) {
+	t.Parallel()
+
+	cv := codeverif.New(4, 5*time.Second, 100)
+	code, err := cv.RequestCode("userID")
+	require.NoError(t, err)
+
+	var wg sync.WaitGroup
+	var successCount int
+	var mu sync.Mutex
+
+	for range 10 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := cv.VerifyCode("userID", code)
+			if err == nil {
+				mu.Lock()
+				successCount++
+				mu.Unlock()
+			}
+		}()
+	}
+
+	wg.Wait()
+	assert.Equal(t, 1, successCount)
 }
